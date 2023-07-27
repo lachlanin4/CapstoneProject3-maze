423c932a394fc4520315f3d9e4f16aa1065188ea
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-07-27 23:34:41 +0100

    Made notes of usable turtle functions

--- graphics.py
+++ graphics.py
@@ -5,6 +5,46 @@ sophia.color('pink')
 sophia.shape('turtle')
 t.Turtle().screen.title('Turtle Escape')
 t.Turtle().screen.bgcolor('#add8e6')
+
+"""
+#Movement functions in turtle
+sophia.position() #shorthand:.pos
+sophia.forward(25) #also accepts negative parameters
+#forward can be shortened to sophia.fd
+sophia.backward(90) #shorthand: .back and .bk
+sophia.right(90) #rotates clockwise. parameter angle in degrees shorthand: .rt
+sophia.left(180) #shorthand:.lt
+sophia.heading() #find out angle turtle is heading
+sophia.goto(30.50) #tells turtle where to go x,y parameters
+sophia.setposition(600,90) #sets turtle position, xy parameters shorthand:setpos
+sophia.setx()#just sets x coordinate leaves y unchanged
+sophia.sety()#sets y coordinate
+sophia.setheading()#sets angle turtle will face shorthand:.seth
+sophia.home()#returns turtle to 0,0 coordinates and it's starting angle
+sophia.dot(10,'green') #puts a dot on screen where turtle is, parameters are size and colour
+sophia.fd(100);sophia.dot(20,'blue');sophia.fd(100) #You cannot draw with dot it is just a dot
+#didn't know that you could use semi colons in python like this?
+
+#Making a stamp, stamps leave a print of your shape in the place your turtle is
+#If you make it as a variable you can use .clearstamp(variableName) to delete it
+stampy =sophia.stamp()
+sophia.fd(50)
+sophia.clearstamp(stampy)
+
+.clearstamps() to delete all stamps
+sophia.hideturtle() short: ht()
+.showturtle() .st()
+
+.pensize parameter is width
+"""
+# s = t.Shape("compound")
+# poly1 = ((0,0),(10,-5),(0,10),(-10,-5))
+# s.addcomponent(poly1, "red", "blue")
+# poly2 = ((0,0),(10,-5),(-10,-5))
+# s.addcomponent(poly2, "blue", "red")
+# t.register_shape("myshape", s)
+# t.shape("myshape")
+
 # for steps in range(100):
 #     for c in ('blue', 'red', 'green'):
 #         t.color(c)
@@ -23,28 +63,6 @@ t.Turtle().screen.bgcolor('#add8e6')
 # sophia.penup()
 # sophia.goto(-160, 100)
 # sophia.pendown()
-
-
-# josh = Turtle()
-# josh.color('green')
-# josh.shape('turtle')
-# josh.penup()
-# josh.goto(-160, 70)
-# josh.pendown()
-
-# damini = Turtle()
-# damini.color('red')
-# damini.shape('turtle')
-# damini.penup()
-# damini.goto(-160, 40)
-# damini.pendown()
-
-# holly = Turtle()
-# holly.color('yellow')
-# holly.shape('turtle')
-# holly.penup()
-# holly.goto(-160,10)
-# holly.pendown()
 
 # from random import randint
 
