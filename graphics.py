1db0f0d1e53bfc14c0373efbcfc757403cfdceb9
Sophiak113 <138770183+Sophiak113@users.noreply.github.com>
2023-07-27 21:48:52 +0100

    Testing Turtle commands

---
+++ graphics.py
@@ -0,0 +1,57 @@
+import turtle as t
+
+sophia = t.Turtle()
+sophia.color('pink')
+sophia.shape('turtle')
+
+# for steps in range(100):
+#     for c in ('blue', 'red', 'green'):
+#         t.color(c)
+#         t.forward(steps)
+#         t.right(30)
+# t.color('red')
+# t.fillcolor('yellow')
+# t.begin_fill()
+# while True:
+#     t.forward(200)
+#     t.left(170)
+#     if abs(t.pos()) < 1:
+#         break
+# t.end_fill()
+
+# sophia.penup()
+# sophia.goto(-160, 100)
+# sophia.pendown()
+
+
+# josh = Turtle()
+# josh.color('green')
+# josh.shape('turtle')
+# josh.penup()
+# josh.goto(-160, 70)
+# josh.pendown()
+
+# damini = Turtle()
+# damini.color('red')
+# damini.shape('turtle')
+# damini.penup()
+# damini.goto(-160, 40)
+# damini.pendown()
+
+# holly = Turtle()
+# holly.color('yellow')
+# holly.shape('turtle')
+# holly.penup()
+# holly.goto(-160,10)
+# holly.pendown()
+
+# from random import randint
+
+# for movement in range(100):
+#   sophia.forward(randint(1,5))
+#   josh.forward(randint(1,5))
+#   damini.forward(randint(1,5))
+#   holly.forward(randint(1,5))
+
+
+input("Press Enter to close")
